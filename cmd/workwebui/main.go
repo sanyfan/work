d3b5a5c43bc778292d977ba7ecc33440b85ed955
fanyang <fanyang@freewheel.tv>
2019-06-17 19:30:44 +0800

    add sentinel to webui

--- cmd/workwebui/main.go
+++ cmd/workwebui/main.go
@@ -7,9 +7,11 @@ import (
 	"os/signal"
 	"strconv"
 	"time"
-
+	"errors"
+	"github.com/FZambia/go-sentinel"
 	"github.com/garyburd/redigo/redis"
 	"github.com/vaporz/work/webui"
+	"strings"
 )
 
 var (
@@ -17,6 +19,7 @@ var (
 	redisDatabase  = flag.String("database", "0", "redis database")
 	redisNamespace = flag.String("ns", "work", "redis namespace")
 	webHostPort    = flag.String("listen", ":5040", "hostport to listen for HTTP JSON API")
+	redisSentinelHosts  = flag.String("sentinel", "", "redis sentinel hostport")
 )
 
 func main() {
@@ -24,6 +27,7 @@ func main() {
 
 	fmt.Println("Starting workwebui:")
 	fmt.Println("redis = ", *redisHostPort)
+	fmt.Println("redis sentinel = ", *redisSentinelHosts)
 	fmt.Println("database = ", *redisDatabase)
 	fmt.Println("namespace = ", *redisNamespace)
 	fmt.Println("listen = ", *webHostPort)
@@ -34,7 +38,11 @@ func main() {
 		return
 	}
 
-	pool := newPool(*redisHostPort, database)
+	pool ,err := createPool(*redisHostPort,  *redisSentinelHosts,database)
+	if err != nil {
+		fmt.Printf("Error: create redis pool err: %v", err)
+		return
+	}
 
 	server := webui.NewServer(*redisNamespace, pool, *webHostPort)
 	server.Start()
@@ -47,6 +55,63 @@ func main() {
 	server.Stop()
 
 	fmt.Println("\nQuitting...")
+}
+
+
+func sentinelHosts(sentinelHostStr string) []string {
+	if strings.TrimSpace(sentinelHostStr) == "" {
+		return []string{}
+	}
+	return strings.Split(sentinelHostStr, ",")
+}
+
+func sentinelDialFunc(hosts []string,db int) func() (redis.Conn, error) {
+	sntnl := &sentinel.Sentinel{
+		Addrs:      hosts,
+		MasterName: "mymaster",
+		Dial: func(addr string) (redis.Conn, error) {
+			timeout := 500 * time.Millisecond
+			c, err := redis.Dial("tcp", addr,
+				redis.DialReadTimeout(timeout), redis.DialWriteTimeout(timeout), redis.DialConnectTimeout(timeout),redis.DialDatabase(db))
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+	}
+	return func() (redis.Conn, error) {
+		masterAddr, err := sntnl.MasterAddr()
+		if err != nil {
+			return nil, err
+		}
+		c, err := redis.Dial("tcp", masterAddr)
+		fmt.Println("redis master address: " + masterAddr)
+		if err != nil {
+			return nil, err
+		}
+		return c, nil
+	}
+}
+
+
+func createPool(addr,sentinel string,database int) (*redis.Pool,error) {
+	dialFunc := func() (redis.Conn, error) { return nil, nil }
+	if len(sentinel) > 0 {
+		sentinelHosts := sentinelHosts(sentinel)
+		dialFunc = sentinelDialFunc(sentinelHosts,database)
+	} else if len(*redisHostPort) > 0 {
+		dialFunc = func() (redis.Conn, error) {
+			return redis.DialURL(addr, redis.DialDatabase(database))
+		}
+	} else {
+		return nil, errors.New("invalid sentinel hosts and host + port")
+	}
+	return &redis.Pool{
+		MaxActive: 3,
+		MaxIdle:   3,
+		Wait:      true,
+		Dial:      dialFunc,
+	},nil
 }
 
 func newPool(addr string, database int) *redis.Pool {
